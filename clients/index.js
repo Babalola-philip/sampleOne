c561e8b15c8f5a26e12aebf15915a0a3a83b8ab1
iamlegend <iamlegendarium@gmail.com>
2024-07-01 19:30:17 +0100

    Signup page working

---
+++ clients/index.js
@@ -0,0 +1,8 @@
+const trackBtn = () =>{
+
+    let url = ''
+    const trackButton = document.getElementById("track").value
+
+    const formData = new FormData();
+    formData.append('track', trackButton);
+}
